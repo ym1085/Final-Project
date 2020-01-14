411b609f80d1bcdc5823df32f96a30d1ad143170
김익주 <@DESKTOP-C77JL5K>
2020-01-15 01:07:47 +0900

    2020-01-15 01:07 시도,구군 셀렉 ajax로 완료 by 김익주

--- delight/src/main/java/com/fp/delight/common/controller/AreaController.java
+++ delight/src/main/java/com/fp/delight/common/controller/AreaController.java
@@ -1,16 +1,22 @@
 package com.fp.delight.common.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.fp.delight.common.model.AreaService;
+import com.fp.delight.excel.model.AreaGugunVO;
 import com.fp.delight.excel.model.AreaSidoVO;
 
 @Controller
@@ -24,11 +30,38 @@ public class AreaController {
 	public String area(Model model) {
 		logger.info("지역 시,도 목록 검색 시작");
 		
-		List<AreaSidoVO> list=areaSerive.selectSido();
-		
+		List<AreaSidoVO> list=new ArrayList<AreaSidoVO>();
+		list=areaSerive.selectSido();
 		model.addAttribute("sido", list);
 		
+
 		return "inc/inc_area";
+	}
+	
+	@RequestMapping(value = "/inc/areagugun.do" ,method = RequestMethod.POST)
+	@ResponseBody
+	public Object areagugun(@RequestBody String sido) {
+		logger.info("ajax 시작 파라미터 sido={}",sido);
+		int last2=sido.lastIndexOf(":\"");
+		int last=sido.lastIndexOf("\"");
+		logger.info("last2={},last={}",last2,last);
+		String ss=sido.substring(last2+2, last);
+		logger.info("ss={}",ss);
+		List<AreaGugunVO> list=areaSerive.gugunSelectBySido(ss);
+		logger.info("list.size()={}",list.size());
+		
+		ArrayList<String> arrList=new ArrayList<String>();
+		for(int i=0;i<list.size();i++) {
+			arrList.add(list.get(i).getGugun());
+		}
+		
+		logger.info("arrList={}",arrList);
+		
+		
+		Map<String, Object> reMap=new HashMap<String, Object>();
+		reMap.put("list", list);
+		
+		return reMap;
 	}
 	
 	
--- delight/src/main/webapp/WEB-INF/views/admin/salesManagement/salesSetting.jsp
+++ delight/src/main/webapp/WEB-INF/views/admin/salesManagement/salesSetting.jsp
@@ -4,15 +4,58 @@
 <%@include file="../inc/adminTop.jsp" %>
 
 <div class="content-wrapper">
-           
+	<div>
+		<form action="" name="searchFrm">
+		<span>장르</span>
+		<select id="type" name="type">
+			<option value="">장르 선택</option>
+			<option value="AAAA">연극</option>
+			<option value="AAAB">뮤지컬</option>
+		</select>
+		 
+		<c:import url="/inc/inc_area.do"></c:import>
+		 
+		<span>시작일</span>
+		<input type="text" id="stdate" name="stdate">
+		 
+		<span>종료일</span>
+		<input type="text" id="eddate" name="eddate">
+		
+		<button type="button">검색</button>
+		</form>
+	</div>
+	
+	
 </div>
 
 <%@include file="../inc/adminBottom.jsp" %>
-
+<style type="text/css">
+form[name=searchFrm] :not(span){
+	margin-left: 20px;
+	margin-right: 20px;
+}
+form[name=searchFrm] span{
+	padding-top: 10px;
+}
+form[name=searchFrm] button{
+	margin: 0;
+}
+</style>
 <script type="text/javascript">
 $(function() {
 	$("#salesManagement").addClass("active");
 	$("#sell").addClass("show");
+	$("form[name=searchFrm] span").addClass("font-weight-bold");
+	$("form[name=searchFrm]").addClass("text-center");
+	$("form[name=searchFrm] *").addClass("text-dark");
+	$("form[name=searchFrm] select").addClass("form-control");
+	$("form[name=searchFrm] select").addClass("form-control-sm");
+	$("form[name=searchFrm] input").addClass("form-control");
+	$("form[name=searchFrm] input").addClass("form-control-sm");
+	$("form[name=searchFrm] button").addClass("btn btn-inverse-success btn-fw btn-sm");
+	$("form[name=searchFrm] select").css("width","200px");
+	$("form[name=searchFrm] input").css("width","200px");
+	$("form[name=searchFrm] *").css("float","left");
 	
 });
 
